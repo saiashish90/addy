2982055927e14c29c5149b4388ff9bec73615bdc
saiashish90 <saiashish90@gmail.com>
2020-08-05 11:31:21 +0530

    Home page

---
+++ src/pages/films.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const films = () => {
+	return <div>helo</div>;
+};
+export default films;
---
+++ src/pages/photofeatures.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const photofeatures = () => {
+	return <div>helo</div>;
+};
+export default photofeatures;
