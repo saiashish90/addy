b12577f3749483b10258bb0f432fc230058bff22
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2021-04-29 16:51:44 +0530

    One page scroll implemented

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Name from "./components/name";
+import ReactPageScroller from "react-page-scroller";
 export default function Home() {
   return (
-    <>
+    <ReactPageScroller>
       <Name></Name>
-      <section style={{ height: "100vh" }}></section>
-    </>
+      <section style={{ height: "100vh", backgroundColor: "#000000" }}></section>
+    </ReactPageScroller>
   );
 }
