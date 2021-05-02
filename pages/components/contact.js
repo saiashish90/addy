a8f9e2c29b349e9c7745435d69ac567174394c67
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2021-05-02 13:17:20 +0530

    Contact page is done

--- pages/components/contact.js
+++ pages/components/contact.js
@@ -3,16 +3,23 @@ export default function eleventh() {
   return (
     <div className={contact.container}>
       <div className={contact.text}>
-        <h1>Work with me</h1>
-        <h2>Email</h2>
+        <span className={contact.h1}>Work with me</span>
+        <span className={contact.h2}>Email</span>
         <span>adithi.chandrashekar@flame.edu.in</span>
-        <h2>Mobile</h2>
+        <span className={contact.h3}>Mobile</span>
         <span>+91 9110289347</span>
         <button>Contact Me</button>
       </div>
       <div className={contact.picture}>
-        <div className={contact.line} />
+        <div className={contact.vectop}>
+          <img src="8th-lines.svg" alt="lines" />
+          <img src="8th-lines.svg" alt="lines" />
+        </div>
         <img src="11th-addy.jpeg" alt="adithi chandrashekar" />
+        <div className={contact.vecbottom}>
+          <img src="8th-lines.svg" alt="lines" />
+          <img src="8th-lines.svg" alt="lines" />
+        </div>
       </div>
     </div>
   );
