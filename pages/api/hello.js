3c20030cf427f286cc02c7500a7b8c50c0698fab
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2021-05-03 17:34:31 +0530

    mail done

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+require("dotenv").config({ path: "../../.env" });
+const nodemailer = require("nodemailer");
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+  if (req.method === "POST") {
+    const { name, email, message } = req.body;
+
+    var transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASS,
+      },
+    });
+    const mailOption = {
+      from: `${email}`,
+      to: `adithic99@gmail.com`,
+      subject: `New mail from ${email}`,
+      text: `
+      ${name} wrote:
+      ${message}
+      `,
+    };
+    transporter.sendMail(mailOption, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("mail send");
+      }
+    });
+    res.send("success");
+  }
+  res.status(200).json({ name: "John Doe" });
+};
--- pages/components/modal.js
+++ pages/components/modal.js
@@ -1,6 +1,6 @@
 import popup from "../../styles/modal.module.scss";
 import { useDisableScroll } from "../../hooks/useDisableScroll";
-export default function modal({ isOpen, children, close }) {
+export default function modal({ isOpen, close }) {
   useDisableScroll(isOpen);
   if (!isOpen) return null;
   return (
@@ -8,18 +8,28 @@ export default function modal({ isOpen, children, close }) {
       <div className={popup.overlay}></div>
       <div className={popup.modal}>
         <div className={popup.close} onClick={close} />
-        <form className={popup.form}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const data = JSON.stringify(Object.fromEntries(new FormData(e.target)));
+            fetch("/api/hello", {
+              method: "POST",
+              headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json",
+              },
+              body: data,
+            });
+            close();
+          }}
+          className={popup.form}>
           <span>Email Me</span>
           <div className={popup.inputgroup}>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="text" placeholder="Message" />
+            <input id="name" name="name" type="text" placeholder="Name" />
+            <input id="email" name="email" type="email" placeholder="Email" />
+            <input id="message" name="message" type="text" placeholder="Message" />
           </div>
-          <button
-            onClick={() => {
-              console.log("submitted");
-            }}
-            className={popup.send}>
+          <button className={popup.send}>
             <span className={popup.but}>Send</span>
           </button>
         </form>
